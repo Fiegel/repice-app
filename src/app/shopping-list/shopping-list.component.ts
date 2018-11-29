61181e72f2812455802e4e02ea81be4f83ab00f4
FIEGEL <jeremy.fiegel@hotmail.fr>
2018-11-29 18:05:26 +0100

    [Lecture 3-52] Creation of the Ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
-  templateUrl: 'shopping-list.component.html'
+  templateUrl: 'shopping-list.component.html',
+  styleUrls: ['shopping-list.component.scss']
 })
 export class ShoppingListComponent {
+  ingredients = [];
 
 }
