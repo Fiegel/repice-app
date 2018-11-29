d283f3f021eb208a5ff78c0416f26b221919c94f
FIEGEL <jeremy.fiegel@hotmail.fr>
2018-11-29 17:44:13 +0100

    [Lecture 3-48] Recipe List component - Add 1 item in html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: 'recipe-list.component.html'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg')
+  ];
 }
