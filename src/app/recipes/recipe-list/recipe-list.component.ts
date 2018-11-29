57c6eea3c43bdfcc816ca47bbc6d2c089bedfe35
FIEGEL <jeremy.fiegel@hotmail.fr>
2018-11-29 17:52:01 +0100

    [Lecture 3-49] Display of the list of recipes with an ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg'),
+    new Recipe('Another Recipe', 'Same description :)', 'https://cdn.pixabay.com/photo/2016/01/08/08/31/paella-1127334_960_720.jpg')
   ];
 }
