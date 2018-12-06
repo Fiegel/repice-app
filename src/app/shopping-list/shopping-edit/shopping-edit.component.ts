a681a6f9b927e5a78faab316f8def3339a455a01
FIEGEL <jeremy.fiegel@hotmail.fr>
2018-12-06 16:39:54 +0100

    [Lecture 6-82] Add ViewChild comm to shopping edit to add new ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: 'shopping-edit.component.html'
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
