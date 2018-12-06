6ff4693f0b242f44e1714e50818a8e29733f988b
FIEGEL <jeremy.fiegel@hotmail.fr>
2018-12-06 17:41:58 +0100

    [Lecture 10-109] Remove comp comm for recipe selection for a new service method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,15 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/09/08/13/16/recipe-2728726_960_720.jpg'),
-    new Recipe('Another Recipe', 'Same description :)', 'https://cdn.pixabay.com/photo/2016/01/08/08/31/paella-1127334_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Gulyas080.jpg/280px-Gulyas080.jpg'),
+    new Recipe('Another Recipe 2', 'Same description :)',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/B%C5%93uf_bourguignon_05.JPG/1024px-B%C5%93uf_bourguignon_05.JPG')
   ];
 
   getRecipes(): Recipe[] {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
@@ -8,6 +8,14 @@ import { RecipeService } from './recipe.service';
   templateUrl: 'recipes.component.html',
   providers: [RecipeService]
 })
-export class RecipesComponent {
-  @Input() selectedRecipe: Recipe;
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
+  }
 }
