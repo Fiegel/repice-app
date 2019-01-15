ae99fbaa9a2de761c462c5ac56ef6a577dc3ec49
FIEGEL <jeremy.fiegel@hotmail.fr>
2019-01-15 17:02:08 +0100

    [Lecture 19-253] Firebase db creation and store recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Component, OnDestroy } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+  saveDataSubscription: Subscription;
 
+  constructor(private dataStorageService: DataStorageService) { }
+
+  onSaveData() {
+    this.saveDataSubscription = this.dataStorageService.storeRecipes()
+      .subscribe((response: Response) => {
+        console.log(response);
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.saveDataSubscription) {
+      this.saveDataSubscription.unsubscribe();
+    }
+  }
 }
