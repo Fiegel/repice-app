b0cb8d7f23855fadadd53662e8faf7849614289f
FIEGEL <jeremy.fiegel@hotmail.fr>
2019-01-21 16:43:58 +0100

    [Lecture 23-301] Use HttpClient instead of Http

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { map } from 'rxjs/operators';
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 
 import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -11,24 +11,22 @@ import { RecipeService } from '../recipes/recipe.service';
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
     private recipeService: RecipeService,
     private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put('https://ng-recipe-book-id.firebaseio.com/recipes.json?auth=' + token,
+    return this.httpClient.put('https://ng-recipe-book-id.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://ng-recipe-book-id.firebaseio.com/recipes.json?auth=' + token)
-      .pipe(map((response: Response) => {
-        const recipes: Recipe[] = response.json();
-
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-id.firebaseio.com/recipes.json?auth=' + token)
+      .pipe(map(recipes => {
         for (const recipe of recipes) {
           if (!recipe['ingredients']) {
             recipe['ingredients'] = [];
