0e02bcf34a706d6f70875435efe3cf01f3a33a40
FIEGEL <jeremy.fiegel@hotmail.fr>
2019-01-21 15:33:40 +0100

    [Lecture 21-285] Home component + Recipes module lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
+import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AuthModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     AppRoutingModule
